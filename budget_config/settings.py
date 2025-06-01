463218eb70fdf3b64a27323e90433fee242a70eb
mcwilk <maciej.wilk04@gmail.com>
2025-06-01 14:52:41 +0200

    WIP

--- budget_config/settings.py
+++ budget_config/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
         'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
